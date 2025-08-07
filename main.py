a5681338709e290ce61a1010b641ae437c6c5639
Jacky <criptinite41@gmail.com>
2025-08-06 22:20:19 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import webbrowser as wb
+
+while True:
+    user_input = input('are you racist? y/n >> ')
+    if user_input == 'n':
+        user_input = input('you lika da black man twerk? absolutely/nah >> ')
+        if user_input == 'absolutely':
+            wb.open('https://c.tenor.com/aACtrJmVYMgAAAAd/tenor.gif')
+        if user_input == 'nah':
+            print('fucking weirdo die')
+    if user_input == 'y':
+        print('Racism detected !!!')
